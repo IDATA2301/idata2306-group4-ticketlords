faaae7e7fb8130c62d10253ec5f639f8bc9954b2
Kristian <kristianty80@gmail.com>
2026-03-03 12:58:27 +0100

    Initial spring boot commit

---
+++ src/main/java/dog/ticketlords/TicketlordsBE/TicketlordsBeApplication.java
@@ -0,0 +1,13 @@
+package dog.ticketlords.TicketlordsBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlordsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketlordsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dog/ticketlords/TicketlordsBE/TicketlordsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dog.ticketlords.TicketlordsBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketlordsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
