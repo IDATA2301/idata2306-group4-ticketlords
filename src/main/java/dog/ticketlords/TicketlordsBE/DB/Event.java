e42d6a68b7800f6b1f5f9854c0907cf207a3c4c7
Jogeir Liavåg <jogeir@tutanota.com>
2026-03-06 16:31:50 +0100

    fix parameters to comply with database structure

--- src/main/java/dog/ticketlords/TicketlordsBE/DB/Event.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DB/Event.java
@@ -20,35 +20,29 @@ public class Event {
   @Setter
   private String host;
   @Setter
-  private String category;
-  @Setter
-  private String country;
-  @Setter
-  private String city;
-  @Setter
-  private String arena;
-  @Setter
-  private String eventDescription;
-  @Setter
-  private int totalClicks;
+  private int categoryID;
   @Setter
   private LocalDate eventDateStart;
   @Setter
   private LocalDate eventDateEnd;
+  @Setter
+  private int venueID;
+  @Setter
+  private String eventDescription;
+  @Setter
+  private int totalClicks;
 
-  public Event(String eventName, int eventID, String host, String category, String country, String city, String arena,
+  public Event(String eventName, int eventID, String host, int categoryID, int venueID,
       String eventDescription, int totalClicks, LocalDate start, LocalDate end) {
     this.eventName = eventName;
     this.eventID = eventID;
     this.host = host;
-    this.category = category;
-    this.country = country;
-    this.city = city;
-    this.arena = arena;
-    this.eventDescription = eventDescription;
-    this.totalClicks = totalClicks; // TODO: Should just be default 0 at creation?
+    this.categoryID = categoryID;
     this.eventDateStart = start;
     this.eventDateEnd = end;
+    this.venueID = venueID;
+    this.eventDescription = eventDescription;
+    this.totalClicks = totalClicks;
 
   }
 }
