8bce7c255618bac030105b6aa391dc0b83a31dc6
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-08 17:17:13 +0100

    Added @annotation for table and column names, and @annotation to make venueId increment

--- src/main/java/dog/ticketlords/TicketlordsBE/DB/EventVenue.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DB/EventVenue.java
@@ -1,7 +1,11 @@
 package dog.ticketlords.TicketlordsBE.DB;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,17 +13,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Getter
+@Table(name = "event_venue")
 public class EventVenue {
 
   @Id
+  @Column(name = "venue_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int venueId;
   @Setter
+  @Column(name = "arena", nullable = false)
   private String arena;
   @Setter
+  @Column(name = "city")
   private String city;
   @Setter
+  @Column(name = "country", nullable = false)
   private String country;
   @Setter
+  @Column(name = "address", nullable = false)
   private String address;
 
   public EventVenue(int venueId, String arena, String city, String country, String address) {
