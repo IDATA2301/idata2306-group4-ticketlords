d80ab8ad42a1b81b6c0c2ee83409271b7548bf65
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-08 14:54:29 +0100

    Created Event entity

--- src/main/java/dog/ticketlords/TicketlordsBE/DB/Event.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DB/Event.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,28 +21,32 @@ public class Event {
   private String eventName;
   @Setter
   private String host;
+  @ManyToOne
+  @JoinColumn(name = "category_id", referencedColumnName = "category_id")
   @Setter
-  private int categoryID;
+  private Category category;
   @Setter
   private LocalDate eventDateStart;
   @Setter
   private LocalDate eventDateEnd;
   @Setter
-  private int venueID;
+  @ManyToOne
+  @JoinColumn(name = "venue_id", referencedColumnName = "venue_id")
+  private EventVenue eventVenue;
   @Setter
   private String eventDescription;
   @Setter
   private int totalClicks;
 
-  public Event(String eventName, int eventID, String host, int categoryID, int venueID,
+  public Event(String eventName, int eventID, String host, Category category, EventVenue eVenue,
       String eventDescription, int totalClicks, LocalDate start, LocalDate end) {
     this.eventName = eventName;
     this.eventID = eventID;
     this.host = host;
-    this.categoryID = categoryID;
+    this.category = category;
     this.eventDateStart = start;
     this.eventDateEnd = end;
-    this.venueID = venueID;
+    this.eventVenue = eVenue;
     this.eventDescription = eventDescription;
     this.totalClicks = totalClicks;
 
