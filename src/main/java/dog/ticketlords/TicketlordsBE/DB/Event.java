eb87201922c1d78fad95a0d70e2c9e1d5ba2c12c
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-08 17:19:29 +0100

    Added @annotation for table and column names.

--- src/main/java/dog/ticketlords/TicketlordsBE/DB/Event.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DB/Event.java
@@ -2,10 +2,12 @@ package dog.ticketlords.TicketlordsBE.DB;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,29 +15,39 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Getter
+@Table(name = "event")
 public class Event {
 
   @Id
+  @Column(name = "event_id")
   private int eventID;
   @Setter
+  @Column(name = "event_name", nullable = false)
   private String eventName;
   @Setter
+  @Column(name = "host")
   private String host;
   @ManyToOne
   @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+  @Column(name = "category")
   @Setter
   private Category category;
   @Setter
+  @Column(name = "event_date_start")
   private LocalDate eventDateStart;
   @Setter
+  @Column(name = "event_date_end")
   private LocalDate eventDateEnd;
   @Setter
   @ManyToOne
   @JoinColumn(name = "venue_id", referencedColumnName = "venue_id")
+  @Column(name = "event_venue")
   private EventVenue eventVenue;
   @Setter
+  @Column(name = "event_description")
   private String eventDescription;
   @Setter
+  @Column(name = "total_clicks", nullable = false)
   private int totalClicks;
 
   public Event(String eventName, int eventID, String host, Category category, EventVenue eVenue,
