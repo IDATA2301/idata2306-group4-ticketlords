758175e090ae50cb1db8a857f52bc8317a48390f
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-08 17:06:12 +0100

    Added @annotation to descripe table and column names

--- src/main/java/dog/ticketlords/TicketlordsBE/DB/BookingSite.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DB/BookingSite.java
@@ -1,7 +1,9 @@
 package dog.ticketlords.TicketlordsBE.DB;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,13 +11,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Entity
+@Table(name = "booking_site")
 public class BookingSite {
 
   @Id
+  @Column(name = "ticket_vendor")
   private String ticketVendor;
   @Setter
+  @Column(name = "website_link")
   private String websiteLink;
   @Setter
+  @Column(name = "booking_site_description")
   private String bookingSiteDescription;
 
   public BookingSite(String ticketVendor, String websiteLink, String bookingSiteDescription) {
