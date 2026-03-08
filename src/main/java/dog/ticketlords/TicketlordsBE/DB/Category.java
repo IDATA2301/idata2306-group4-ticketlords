0830812efbbaf9842e35be6995ac92a8161f328b
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-08 14:53:36 +0100

    Created Category entity

--- src/main/java/dog/ticketlords/TicketlordsBE/DB/Category.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DB/Category.java
@@ -3,11 +3,12 @@ package dog.ticketlords.TicketlordsBE.DB;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Category {
 
   @Id
