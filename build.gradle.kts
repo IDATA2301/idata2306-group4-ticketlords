285d2ca5d5bbc49d7f2cdada5cab85777cf35fdc
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-10 19:33:38 +0100

    Added value validation dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 extra["springAiVersion"] = "2.0.0-M2"
 
 dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
