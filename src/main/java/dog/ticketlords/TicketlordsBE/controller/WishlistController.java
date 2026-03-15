2760be075cf19596b9611fe164ee866e9e8dcf30
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-03-15 20:23:49 +0100

    Inject wishlist service on creation

--- src/main/java/dog/ticketlords/TicketlordsBE/controller/WishlistController.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/controller/WishlistController.java
@@ -1,20 +1,26 @@
 package dog.ticketlords.TicketlordsBE.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import dog.ticketlords.TicketlordsBE.repositories.WishlistRepository;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import dog.ticketlords.TicketlordsBE.entity.Wishlist;
+import dog.ticketlords.TicketlordsBE.repositories.WishlistRepository;
+import dog.ticketlords.TicketlordsBE.service.WishlistService;
+
+@RestController
 public class WishlistController {
 
   @Autowired
   private WishlistRepository wishlistRepository;
 
-  public WishlistController() {
+  private WishlistService wishlistService;
 
+  public WishlistController(WishlistService wlService) {
+    this.wishlistService = wlService;
   }
 
-  // TODO: Remove later, just to demonstrate.
-  public void test() {
-    wishlistRepository.count();
-
-  }
 }
