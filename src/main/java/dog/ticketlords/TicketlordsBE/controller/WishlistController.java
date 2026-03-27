023acdf4c0bddfa54fdd19e9be1422a5d71eb6ad
JogeirNTNU <jjliavaa@stud.ntnu.no>
2026-03-27 15:57:31 +0100

    add some services, mostly unfinished

--- src/main/java/dog/ticketlords/TicketlordsBE/controller/WishlistController.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/controller/WishlistController.java
@@ -12,15 +12,41 @@ import dog.ticketlords.TicketlordsBE.repositories.WishlistRepository;
 import dog.ticketlords.TicketlordsBE.service.WishlistService;
 
 @RestController
+@RequestMapping("/api/wishlists")
 public class WishlistController {
 
-  @Autowired
-  private WishlistRepository wishlistRepository;
-
-  private WishlistService wishlistService;
+  private final WishlistService wishlistService;
 
   public WishlistController(WishlistService wlService) {
     this.wishlistService = wlService;
   }
+  
+  @GetMapping
+  public List<Wishlist> getAll() {
+    return wishlistService.getAll();
+  }
 
+  @GetMapping("/users/{userId}")
+  public List<Wishlist> getAllForUser(@PathVariable int userId) {
+    return wishlistService.getAllForUser(userId);
+  }
+
+  @GetMapping("/users/{userId}/events/{eventId}")
+  public Optional<Wishlist> getWish(@PathVariable int userId, @PathVariable int eventId) {
+    return wishlistService.getWish(userId, eventId);
+  }
+
+  @PostMapping("/users/{userId}/events/{eventId}")
+  public void insertOneIntoDatabase(@PathVariable int userId, @PathVariable int eventId) {
+    wishlistService.insertOneIntoDatabase(userId, eventId);
+  }
+
+  @DeleteMapping("/users/{userId}/events/{eventId}")
+  public ResponseEntity<Void> remove(@PathVariable int userId, @PathVariable int eventId) {
+    boolean removed = wishlistService.remove(userId, eventId);
+    if (!removed) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.noContent().build();
+  }
 }
