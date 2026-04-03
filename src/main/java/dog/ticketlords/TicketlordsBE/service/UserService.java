6ea7fc42fd2a6f850a7a6a43f565e2790af6afee
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-03 09:34:11 +0200

    Made insert return boolean, instead of void.

--- src/main/java/dog/ticketlords/TicketlordsBE/service/UserService.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/service/UserService.java
@@ -11,8 +11,10 @@ import dog.ticketlords.TicketlordsBE.repositories.UnregisteredUserRepository;
 import dog.ticketlords.TicketlordsBE.repositories.RegisteredUserRepository;
 
 /**
- * Service to handle operations directly with database concerning {@link RegisteredUser}.
- * Includes methods like getting, inserting and removing users from the database.
+ * Service to handle operations directly with database concerning
+ * {@link RegisteredUser}.
+ * Includes methods like getting, inserting and removing users from the
+ * database.
  */
 @Service
 @Transactional
@@ -24,8 +26,10 @@ public class UserService {
   /**
    * Creates a new service for RegisteredUser operations.
    * 
-   * @param regUserRepo the repository to perform persistence operations for {@link RegisteredUser}
-   * @param unregUserRepo the repository to perform persistence operations for {@link UnregisteredUser}
+   * @param regUserRepo   the repository to perform persistence operations for
+   *                      {@link RegisteredUser}
+   * @param unregUserRepo the repository to perform persistence operations for
+   *                      {@link UnregisteredUser}
    */
   public UserService(RegisteredUserRepository regUserRepo, UnregisteredUserRepository unregUserRepo) {
     this.regUserRepo = regUserRepo;
@@ -34,6 +38,7 @@ public class UserService {
 
   /**
    * Returns an RegisteredUser from database.
+   * 
    * @param userId the id of the user
    * @return an RegisteredUser from database as Optional
    */
@@ -50,6 +55,7 @@ public class UserService {
   /**
    * Creates and inserts a new UnregisteredUser into the database.
    * The user_id is generated automatically by the database.
+   * 
    * @return the saved UnregisteredUser with generated id
    */
   public void insertUnregisteredUserToDatabase() {
@@ -61,27 +67,31 @@ public class UserService {
    * Checks if {@link UnregisteredUser} does exists.
    * 
    * @param user the registered user to insert into the database
-   * @throws IllegalArgumentException if the associated UnregisteredUser is not found
+   * @throws IllegalArgumentException if the associated UnregisteredUser is not
+   *                                  found
    */
-  public void insertRegisteredUserToDatabase(RegisteredUser user) {
+  public boolean insertRegisteredUserToDatabase(RegisteredUser user) {
     if (this.getUnregUser(user.getUnregisteredUser().getUId()).isEmpty()) {
-      throw new IllegalArgumentException("UnregisteredUser not found");
+      return false;
     }
     this.regUserRepo.save(user);
+    return true;
   }
 
   /**
    * Delete a user from the database.
    * 
    * @param userId the id of the user
-   * @throws IllegalArgumentException if the associated {@link RegisteredUser} is not found
+   * @throws IllegalArgumentException if the associated {@link RegisteredUser} is
+   *                                  not found
    */
-  public void deleteRegisteredUserFromDatabase(int userId) {
+  public boolean deleteRegisteredUserFromDatabase(int userId) {
     Optional<RegisteredUser> user = getRegUser(userId);
     if (user.isEmpty()) {
-      throw new IllegalArgumentException("RegisteredUser not found");
+      return false;
     }
     this.regUserRepo.delete(user.get());
+    return true;
   }
 
 }
