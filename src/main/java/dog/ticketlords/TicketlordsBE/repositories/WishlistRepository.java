006cbb8291db2fc5f709d1c01ea585bceb0a0faf
Apeskit <kritver@stud.ntnu.no>
2026-04-04 15:59:07 +0200

    New self defined method to find wishlistings by user's id

--- src/main/java/dog/ticketlords/TicketlordsBE/repositories/WishlistRepository.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/repositories/WishlistRepository.java
@@ -1,9 +1,13 @@
 package dog.ticketlords.TicketlordsBE.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dog.ticketlords.TicketlordsBE.entity.Wishlist;
 import dog.ticketlords.TicketlordsBE.entity.WishlistId;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, WishlistId> {
+
+  List<Wishlist> findAllByIdUserId(long userId);
 }
