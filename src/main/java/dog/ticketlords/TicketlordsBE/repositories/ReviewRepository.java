41c2b5976d90adc82c1dddf5f94574bc71a72801
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-06 16:16:15 +0200

    Added method to find all review, and inserting if user hasnt made review of a specified venue yet.

--- src/main/java/dog/ticketlords/TicketlordsBE/repositories/ReviewRepository.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/repositories/ReviewRepository.java
@@ -1,9 +1,14 @@
 package dog.ticketlords.TicketlordsBE.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dog.ticketlords.TicketlordsBE.entity.Review;
 import dog.ticketlords.TicketlordsBE.entity.ReviewId;
 
 public interface ReviewRepository extends JpaRepository<Review, ReviewId> {
+  List<Review> findAllByUser_UserId(long userId);
+
+  boolean existsByUser_UserIdAndBookingSiteTicketVendorIgnoreCaseContaining(long userId, String vendorName);
 }
