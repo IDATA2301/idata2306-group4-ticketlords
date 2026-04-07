711e77b966f40ee1dfacb2ab80488d625774b763
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-07 11:18:04 +0200

    new methods to find reviews by

--- src/main/java/dog/ticketlords/TicketlordsBE/repositories/ReviewRepository.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/repositories/ReviewRepository.java
@@ -11,4 +11,8 @@ public interface ReviewRepository extends JpaRepository<Review, ReviewId> {
   List<Review> findAllByUser_UserId(long userId);
 
   boolean existsByUser_UserIdAndBookingSiteTicketVendorIgnoreCaseContaining(long userId, String vendorName);
+
+  List<Review> findAllByBookingSite_TicketVendorIgnoreCaseContaining(String vendorNameSubstring);
+
+  List<Review> findByBookSite_TicketVendorIgnoreCase(String vendorName);
 }
