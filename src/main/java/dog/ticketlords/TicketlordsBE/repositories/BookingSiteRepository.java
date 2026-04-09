81649e7adf818d7fb785e5a117d98c1775398ed9
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-09 17:15:32 +0200

    Added method

--- src/main/java/dog/ticketlords/TicketlordsBE/repositories/BookingSiteRepository.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/repositories/BookingSiteRepository.java
@@ -3,6 +3,9 @@ package dog.ticketlords.TicketlordsBE.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dog.ticketlords.TicketlordsBE.dbentity.BookingSite;
+
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,5 +14,7 @@ public interface BookingSiteRepository extends JpaRepository<BookingSite, Long> 
   // May be unnecessary, because apparently Sorubg Data Jpa generates some
   // implementations for us. Look into later.
   Page<BookingSite> findByTicketVendor(long vendorId, Pageable pageable);
+
+  List<BookingSite> findAllByTicketVendorContainingIgnoreCase(String nameSubstring);
 
 }
