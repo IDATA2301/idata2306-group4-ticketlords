2d50aa87ff3e6ae90f0d3f72ea597005bb76ff93
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-09 18:06:22 +0200

    added method to get the amount of clicks, based on eventId

--- src/main/java/dog/ticketlords/TicketlordsBE/repositories/EventClicksRepository.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/repositories/EventClicksRepository.java
@@ -6,4 +6,5 @@ import dog.ticketlords.TicketlordsBE.dbentity.EventClicks;
 import dog.ticketlords.TicketlordsBE.dbentity.EventClicksId;
 
 public interface EventClicksRepository extends JpaRepository<EventClicks, EventClicksId> {
+  long countByEvent_EventId(long eventId);
 }
