7733773d2bf3836a8d59cd917d049e9ad321c1d7
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-10 15:51:02 +0200

    created some methods for price alert

--- src/main/java/dog/ticketlords/TicketlordsBE/service/PriceAlertService.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/service/PriceAlertService.java
@@ -1,7 +1,13 @@
 package dog.ticketlords.TicketlordsBE.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import dog.ticketlords.TicketlordsBE.dbentity.PriceAlert;
 import dog.ticketlords.TicketlordsBE.repositories.PriceAlertRepository;
 
+@Service
 public class PriceAlertService {
 
   private PriceAlertRepository priceAlertRepository;
@@ -10,4 +16,24 @@ public class PriceAlertService {
     this.priceAlertRepository = priceAlertRepository;
   }
 
+  public boolean createNewPriceAlert() {
+    return true;
+  }
+
+  public List<PriceAlert> getAllPriceAlertsByUserId(long userId) {
+    return this.priceAlertRepository.findAll();
+  }
+
+  public boolean updateAlert(PriceAlert updatedPriceAlert) {
+    if (this.priceAlertRepository.existsById(updatedPriceAlert.getAlertId())) {
+      PriceAlert databaseAlert = this.priceAlertRepository.findById(updatedPriceAlert.getAlertId()).get();
+      if (updatedPriceAlert.getIsActive() != null) {
+        databaseAlert.setActive(updatedPriceAlert.getIsActive());
+      }
+      if (updatedPriceAlert.getTargetPrice() != null) {
+        databaseAlert.setTargetPrice(updatedPriceAlert.getTargetPrice());
+      }
+
+    }
+  }
 }
