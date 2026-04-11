1e18d0ecf8f91dd26969ff4ff415dae95356d6c3
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-11 15:51:32 +0200

    Added methods to find categories by name.

--- src/main/java/dog/ticketlords/TicketlordsBE/repositories/CategoryRepository.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/repositories/CategoryRepository.java
@@ -1,8 +1,13 @@
 package dog.ticketlords.TicketlordsBE.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dog.ticketlords.TicketlordsBE.dbentity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+  Optional<Category> findByCategoryName(String categoryName);
+
+  List<Category> findByCategoryNameContainingIgnoreCase(String substringCategoryName);
 }
