ba862b799315db4e1089234d995c5e38692a7dba
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-13 13:09:15 +0200

    Javadoc

--- src/main/java/dog/ticketlords/TicketlordsBE/service/EventVenueService.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/service/EventVenueService.java
@@ -18,14 +18,37 @@ public class EventVenueService {
     this.eventVenueRepo = eventVenueRepo;
   }
 
+  /**
+   * Gets an optional of an event venue based on its id.
+   *
+   * @param eId the id of the {@link EventVenue} to find.
+   * @return an optional containing the EventVenue if it exists, optional
+   *         containing null otherwise.
+   */
   public Optional<EventVenue> getEventVenueById(long eId) {
     return this.eventVenueRepo.findById(eId);
   }
 
+  /**
+   * Gets a list of {@link EventVenue} based on the country and the city they are
+   * located.
+   *
+   * @param country the country the EventVenue is located.
+   * @param city    the city the EventVenue is located.
+   *
+   * @return A list of all EventVenues matching whose country and city match the
+   *         params. If none match, return an empty list.
+   */
   public List<EventVenue> getEventVenueByLocation(String country, String city) {
     return this.eventVenueRepo.findAllByCountryIgnoreCaseAndCityIgnoreCase(country, city);
   }
 
+  /**
+   * Adds an {@link EventVenue} to the database.
+   *
+   * @param an {@link EventVenue} object to insert to the database.
+   * @return true if successfully added. False otherwise.
+   */
   public boolean addEventVenueToDatabase(EventVenue eventVenue) {
     Example<EventVenue> example = Example.of(eventVenue);
     if (!this.eventVenueRepo.exists(example)) {
@@ -36,10 +59,27 @@ public class EventVenueService {
     }
   }
 
+  /**
+   * Gets an optional of an {@link EventVenue} based on its address.
+   *
+   * @param venueAddress the address where the venue is located.
+   * @return An optional containing the EventVenue if it exists, optional
+   *         containing null otherwise.
+   */
   public Optional<EventVenue> getVenueByAddress(String venueAddress) {
     return this.eventVenueRepo.findByAddressIgnoreCase(venueAddress);
   }
 
+  /**
+   * Gets an {@link EventVenue} reference from the database based on an incoming
+   * EventVenue's id field.
+   * Then updates the database event venue reference with the incoming
+   * EventVenue's fields, and inserts
+   * the updated EventVenue back into the database.
+   *
+   * @param updatedEVenue the EventVenue to update the database instance with.
+   * @return true if successfully updated, false otherwise.
+   */
   public boolean updateEventVenue(EventVenue updatedEVenue) {
     if (this.eventVenueRepo.existsById(updatedEVenue.getVenueId())) {
       EventVenue databaseEVenue = this.eventVenueRepo.getReferenceById(updatedEVenue.getVenueId());
