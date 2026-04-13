b766575cb8d9a013e19156f9ff348cb557bd4183
Håkon <haakmae@stud.ntnu.no>
2026-04-13 16:14:40 +0200

    added methods in CategoryController for retrieving all events and retrieve based on ID

--- src/main/java/dog/ticketlords/TicketlordsBE/controller/CategoryController.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/controller/CategoryController.java
@@ -1,9 +1,21 @@
 package dog.ticketlords.TicketlordsBE.controller;
 
+import dog.ticketlords.TicketlordsBE.dbentity.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import dog.ticketlords.TicketlordsBE.service.CategoryService;
 
+import java.util.List;
+
+/**
+ * REST controller for category management operations.
+ *
+ * Handles GET, POST, and DELETE requests for managing categories in the database.
+ * Provides endpoints to retrieve categories...
+ */
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
@@ -14,4 +26,35 @@ public class CategoryController {
     this.categoryService = categoryService;
   }
 
+  /**
+   * Retrieves all categories from the database.
+   *
+   * @return ResponseEntity containing a list of all categories, or not found if no categories exist.
+   */
+  @GetMapping("/")
+  public ResponseEntity<List<Category>> getAllEvents() {
+    if (this.categoryService.getAllCategories().size() >= 1) {
+      return ResponseEntity.ok(this.categoryService.getAllCategories());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  /**
+   * Retrieves a specific category using event ID.
+   *
+   * @param categoryId the ID of the event to retrieve
+   * @return ResponseEntity containing the category, or not found if event does not exist
+   */
+  @GetMapping("/categories/{categoryId")
+  public ResponseEntity<Category> getCategory(@PathVariable long categoryId) {
+    if (this.categoryService.getCategoryByCategoryId(categoryId).isPresent()) {
+      return ResponseEntity.ok(categoryService.getCategoryByCategoryId(categoryId).get());
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+
+
+
+  }
 }
