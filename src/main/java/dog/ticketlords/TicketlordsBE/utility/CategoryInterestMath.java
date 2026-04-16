8d3e7df19398aeb1e3c6a5903e73aabc1f54ceb8
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-16 15:03:31 +0200

    Fixed bug in formula, so it gives correct value. Added clock to help with testing.

--- src/main/java/dog/ticketlords/TicketlordsBE/utility/CategoryInterestMath.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/utility/CategoryInterestMath.java
@@ -1,6 +1,7 @@
 package dog.ticketlords.TicketlordsBE.utility;
 
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -10,6 +11,11 @@ import java.time.temporal.ChronoUnit;
  */
 public class CategoryInterestMath {
   private static final int BASE_SAME_DAY_SCORE = 10;
+  private final Clock clock;
+
+  public CategoryInterestMath(Clock clock) {
+    this.clock = clock;
+  }
 
   /**
    * Retrieves the strength of a specific interest, based on the date since said
@@ -19,8 +25,8 @@ public class CategoryInterestMath {
    * @return a value between 10 and 0, depending on the date. If the date is in
    *         the future, or more than 365 days in the past, return 0.
    */
-  public static BigDecimal getScoreByDate(LocalDateTime inputTime) {
-    long daysSinceNow = ChronoUnit.DAYS.between(inputTime, LocalDateTime.now());
+  public BigDecimal getScoreByDate(LocalDateTime inputTime) {
+    long daysSinceNow = ChronoUnit.DAYS.between(inputTime, LocalDateTime.now(this.clock));
     if (daysSinceNow >= 365 || daysSinceNow < 0) {
       return BigDecimal.ZERO;
     } else {
@@ -36,7 +42,7 @@ public class CategoryInterestMath {
    *                     now.
    * @return a scaling value between 0.0 and 1.0.
    */
-  private static double scalingCalculator(long daysSinceNow) {
-    return daysSinceNow * (-1 / (Math.log(366)) * Math.log(daysSinceNow + 1) + 1);
+  private double scalingCalculator(long daysSinceNow) {
+    return (-1 / (Math.log(366)) * Math.log(daysSinceNow + 1) + 1);
   }
 }
