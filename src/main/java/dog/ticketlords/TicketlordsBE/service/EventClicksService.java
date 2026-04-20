bc0fc599b2176d93dea6f3db53d6db3c85334d5b
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-04-20 14:58:37 +0200

    Added check for @getEventClicksForEvent. Added one minute restriction for @recordClick so it cannot update the lastInteraction date, unless a minute or more has passed, which makes the event not updateable more than once per minute per user, by the database trigger.

--- src/main/java/dog/ticketlords/TicketlordsBE/service/EventClicksService.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/service/EventClicksService.java
@@ -1,5 +1,7 @@
 package dog.ticketlords.TicketlordsBE.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -21,11 +23,25 @@ public class EventClicksService {
   }
 
   /**
-   * Records a user's click on an event.
+   * Records a user's click on an event if it doesnt exist already. If a previous
+   * click does exist however,
+   * simply update the last interacted at field, with now.
+   *
+   * @param event The {@link Event} that was clicked.
+   * @param user  the {@link UnregisteredUser} that clicked the event.
    */
   public void recordClick(Event event, UnregisteredUser user) {
-    if (!hasUserClickedEvent(event, user)) {
+    LocalDateTime now = LocalDateTime.now();
+    LocalDateTime lastClickTime = this.eventClicksRepository.findLastInteraction(event.getEventId(), user.getUId());
+    boolean hasPassedOneMinuteOrMore = ChronoUnit.MINUTES.between(lastClickTime, now) >= 1;
+
+    if (!this.eventClicksRepository.existsById(new EventClicksId(event.getEventId(), user.getUId()))) {
       this.eventClicksRepository.save(new EventClicks(event, user));
+    } else if (hasPassedOneMinuteOrMore) {
+      EventClicks ec = this.eventClicksRepository
+          .getReferenceById(new EventClicksId(event.getEventId(), user.getUId()));
+      ec.setLastInteraction(now);
+      this.eventClicksRepository.save(ec);
     }
   }
 
@@ -40,34 +56,21 @@ public class EventClicksService {
   }
 
   /**
-   * Checks whether a user has already clicked on an event.
-   * 
-   * @param event    the event to check a click against.
-   * @param uRegUser the unregisteredUser to check if has clicked an event.
-   *
-   * @return true if an EventClicksId with event and uRegUser already exists,
-   *         false otherwise.
-   */
-  private boolean hasUserClickedEvent(Event event, UnregisteredUser uRegUser) {
-    EventClicksId id = new EventClicksId(event.getEventId(), uRegUser.getUId());
-    if (this.eventClicksRepository.existsById(id)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  /**
    * Finds all the recorded clicks for a specified event.
    *
    * @param eventId the event to find clicks from.
    * @return A list containing all EventClick objects. Empty list if no records in
    *         the database.
    */
-  public List<EventClicks> getClicksForEvent(long eventId) {
-    return this.eventClicksRepository.findAllByEvent_EventId(eventId);
+  public List<EventClicks> getEventClicksForEvent(long eventId) {
+    if (this.eventClicksRepository.existsByEvent_EventId(eventId)) {
+      return this.eventClicksRepository.findAllByEvent_EventId(eventId);
+    } else {
+      return List.of();
+    }
   }
 
+  // TODO: Think we should remove this. Use UserInterest's method instead.
   /**
    * Retrieves aggregated click data for a user per category. Maps user interests
    * based on the number of events the user has clicked in each category.
