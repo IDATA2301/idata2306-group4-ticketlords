b45418d09fe838d1b8abd60b72facff4646da1bb
JogeirNTNU <jjliavaa@stud.ntnu.no>
2026-05-06 14:34:39 +0200

    import jwt and dotenv

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,10 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
   implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("io.github.cdimascio:dotenv-java:3.0.0")
+  implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+  runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+  runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
 
 dependencyManagement {
