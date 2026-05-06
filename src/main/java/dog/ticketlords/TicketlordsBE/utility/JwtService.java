0d288aef74a7624217a9bd45683c7fac215d5d4c
JogeirNTNU <jjliavaa@stud.ntnu.no>
2026-05-06 14:51:42 +0200

    handles jwt token generation

---
+++ src/main/java/dog/ticketlords/TicketlordsBE/utility/JwtService.java
@@ -0,0 +1,31 @@
+package dog.ticketlords.TicketlordsBE.utility;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+  
+  @Value("${jwt.secret}")
+  private String secretKey;
+  
+  @Value("${jwt.expiration:86400000}")
+  private long expirationTime;
+  
+  public String generateToken(String email) {
+    SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    return Jwts.builder()
+      .subject(email)
+      .issuedAt(new Date())
+      .expiration(new Date(System.currentTimeMillis() + expirationTime))
+      .signWith(key, Jwts.SIG.HS512)
+      .compact();
+  }
+}
