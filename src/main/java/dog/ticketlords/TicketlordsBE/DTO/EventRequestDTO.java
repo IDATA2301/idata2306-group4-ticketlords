ffe82fcf45aa16070dabcc93d19f8b39b439e25b
Kristian Tverderøy <kristian.tverderoy@gmail.com>
2026-05-12 18:07:36 +0200

    Changed local date to local date time

--- src/main/java/dog/ticketlords/TicketlordsBE/DTO/EventRequestDTO.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/DTO/EventRequestDTO.java
@@ -1,14 +1,14 @@
 package dog.ticketlords.TicketlordsBE.DTO;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record EventRequestDTO(
     String eventName,
     String host,
     long categoryId,
-    LocalDate eventDateStart,
-    LocalDate eventDateEnd,
+    LocalDateTime eventDateStart,
+    LocalDateTime eventDateEnd,
     long venueId,
     String eventDescription,
-    String imgPathUrl
-) {}
+    String imgPathUrl) {
+}
