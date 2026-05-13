6e6f9bbc91832a4e0a55fb4b34633ab7d4a783f2
JogeirNTNU <jjliavaa@stud.ntnu.no>
2026-05-13 16:16:30 +0200

    Fix image url

--- src/main/java/dog/ticketlords/TicketlordsBE/service/ImageStorageService.java
+++ src/main/java/dog/ticketlords/TicketlordsBE/service/ImageStorageService.java
@@ -2,9 +2,11 @@ package dog.ticketlords.TicketlordsBE.service;
 
 import java.io.IOException;
 import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -14,11 +16,15 @@ public class ImageStorageService {
 
   private final S3Client s3;
   private final String bucket;
+  private final String endpoint;
 
-  public ImageStorageService(S3Client s3, @Value("${spaces.bucket}") String bucket) {
+  public ImageStorageService(S3Client s3, 
+                           @Value("${spaces.bucket}") String bucket,
+                           @Value("${spaces.endpoint}") String endpoint) {
     this.s3 = s3;
     this.bucket = bucket;
-  }
+    this.endpoint = endpoint;
+}
 
   public String upload(MultipartFile file) throws IOException {
     String key = "images/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
@@ -33,6 +39,6 @@ public class ImageStorageService {
                       .build(),
             RequestBody.fromBytes(bytes)
     );
-    return key;
+    return this.endpoint + this.bucket + key;
   }
 }
